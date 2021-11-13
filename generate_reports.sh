bb7ea5efd3a1280f4a342c1f8de01cacf899d429
loujine <loujine@protonmail.com>
2021-11-13 22:39:42 +0000

    shellcheck

--- generate_reports.sh
+++ generate_reports.sh
@@ -18,14 +18,14 @@ for script in *sql
 do
     mkdir -p ../$OUTPUTDIR
     OUTPUTFILE="../$OUTPUTDIR/${script%.sql}.md"
-    head -1 $script > $OUTPUTFILE
-    sed -i 's/--/##/' $OUTPUTFILE
-    echo "" >> $OUTPUTFILE
-    psql postgres://$PGPARAMS -f $script >> $OUTPUTFILE
-    sed -i 's/+--/|--/g' $OUTPUTFILE
-    sed -i 's/--+/--|/g' $OUTPUTFILE
-    sed -i '/^Time/d' $OUTPUTFILE
-    sed -i 's/^\(([0-9]* rows\)/\n\1/' $OUTPUTFILE
+    head -1 "$script" > "$OUTPUTFILE"
+    sed -i 's/--/##/' "$OUTPUTFILE"
+    echo "" >> "$OUTPUTFILE"
+    psql postgres://"$PGPARAMS" -f "$script" >> "$OUTPUTFILE"
+    sed -i 's/+--/|--/g' "$OUTPUTFILE"
+    sed -i 's/--+/--|/g' "$OUTPUTFILE"
+    sed -i '/^Time/d' "$OUTPUTFILE"
+    sed -i 's/^\(([0-9]* rows\)/\n\1/' "$OUTPUTFILE"
 done
 popd
 
