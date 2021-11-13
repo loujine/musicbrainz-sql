ab1f614b50cc00ea8f2dcf3948bf5194465a352e
loujine <loujine@protonmail.com>
2021-11-13 14:32:47 +0000

    Skip false positives in link_duplicates_recording_work report
    False positives due to duplicate composer-work relations

--- queries/link_duplicates_recording_work.sql
+++ queries/link_duplicates_recording_work.sql
@@ -1,22 +1,29 @@
--- duplicate relationships between work and recording (filtered on main composers)
+-- duplicate relationships (>= 4) between work and recording (filtered on main composers)
 
+-- >=4
 -- 2020-09-16: 41
+-- 2021-10-16: 55
+-- 2021-11-07: 0
 
 WITH
+composer_work AS (
+    SELECT DISTINCT
+        work.id,
+        work.gid
+    FROM
+        composer
+        INNER JOIN l_artist_work AS law ON composer.id = entity0
+        INNER JOIN work ON work.id = entity1
+),
 results AS (
     SELECT
-        work.gid AS wmbid,
+        composer_work.gid AS wmbid,
         r.gid AS rmbid,
         r.name
     FROM
-        composer
-        /* artist AS composer */
-        INNER JOIN l_artist_work AS law ON composer.id = entity0
-        INNER JOIN work ON work.id = entity1
-        INNER JOIN l_recording_work AS lrw ON work.id = lrw.entity1
+        composer_work
+        INNER JOIN l_recording_work AS lrw ON composer_work.id = lrw.entity1
         INNER JOIN recording AS r ON r.id = lrw.entity0
-    /* WHERE */
-    /*     composer.begin_date_year < 1900 */
 )
 SELECT
     -- to_work_url(wmbid),
