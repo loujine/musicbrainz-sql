842cbaf23348e17e8e999d740f7bf0ec2cce86a8
loujine <loujine@protonmail.com>
2021-11-27 19:02:32 +0000

    Restrict same discid/different recordings to subscribed artists

--- queries/discid_linked_to_releases_with_different_recordings.sql
+++ queries/discid_linked_to_releases_with_different_recordings.sql
@@ -20,16 +20,19 @@ results AS (
     SELECT
         toc.discid,
         t.name AS track,
+        a.sort_name AS name,
         to_recording_url(r.gid) AS url
     FROM
         toc
         INNER JOIN medium_cdtoc AS mc ON mc.cdtoc = toc.id
         INNER JOIN track AS t ON t.medium = mc.medium
         INNER JOIN recording AS r ON t.recording = r.id
+        INNER JOIN l_artist_recording AS lar ON lar.entity1 = r.id
+        INNER JOIN pianist AS a ON lar.entity0 = a.id
     WHERE
-        t.position = 20
+        t.position = 6
     GROUP BY
-        toc.discid, t.name, r.gid
+        toc.discid, t.name, a.sort_name, r.gid
 )
 SELECT
     to_cdtoc_url(discid) AS discid,
@@ -40,3 +43,127 @@ GROUP BY
     discid
 HAVING
     COUNT(DISTINCT(url)) > 1
+;
+
+WITH
+toc AS (
+    SELECT
+        c.id, c.discid
+    FROM
+        cdtoc AS c
+        INNER JOIN medium_cdtoc ON c.id = medium_cdtoc.cdtoc
+    GROUP BY
+        c.id, c.discid
+    HAVING
+        COUNT(c.id) > 1
+),
+results AS (
+    SELECT
+        toc.discid,
+        t.name AS track,
+        a.sort_name AS name,
+        to_recording_url(r.gid) AS url
+    FROM
+        toc
+        INNER JOIN medium_cdtoc AS mc ON mc.cdtoc = toc.id
+        INNER JOIN track AS t ON t.medium = mc.medium
+        INNER JOIN recording AS r ON t.recording = r.id
+        INNER JOIN l_artist_recording AS lar ON lar.entity1 = r.id
+        INNER JOIN violinist AS a ON lar.entity0 = a.id
+    WHERE
+        t.position = 6
+    GROUP BY
+        toc.discid, t.name, a.sort_name, r.gid
+)
+SELECT
+    to_cdtoc_url(discid) AS discid,
+    ARRAY_AGG(track) AS track_names
+FROM
+    results
+GROUP BY
+    discid
+HAVING
+    COUNT(DISTINCT(url)) > 1
+;
+
+WITH
+toc AS (
+    SELECT
+        c.id, c.discid
+    FROM
+        cdtoc AS c
+        INNER JOIN medium_cdtoc ON c.id = medium_cdtoc.cdtoc
+    GROUP BY
+        c.id, c.discid
+    HAVING
+        COUNT(c.id) > 1
+),
+results AS (
+    SELECT
+        toc.discid,
+        t.name AS track,
+        a.sort_name AS name,
+        to_recording_url(r.gid) AS url
+    FROM
+        toc
+        INNER JOIN medium_cdtoc AS mc ON mc.cdtoc = toc.id
+        INNER JOIN track AS t ON t.medium = mc.medium
+        INNER JOIN recording AS r ON t.recording = r.id
+        INNER JOIN l_artist_recording AS lar ON lar.entity1 = r.id
+        INNER JOIN quartet AS a ON lar.entity0 = a.id
+    WHERE
+        t.position = 6
+    GROUP BY
+        toc.discid, t.name, a.sort_name, r.gid
+)
+SELECT
+    to_cdtoc_url(discid) AS discid,
+    ARRAY_AGG(track) AS track_names
+FROM
+    results
+GROUP BY
+    discid
+HAVING
+    COUNT(DISTINCT(url)) > 1
+;
+
+WITH
+toc AS (
+    SELECT
+        c.id, c.discid
+    FROM
+        cdtoc AS c
+        INNER JOIN medium_cdtoc ON c.id = medium_cdtoc.cdtoc
+    GROUP BY
+        c.id, c.discid
+    HAVING
+        COUNT(c.id) > 1
+),
+results AS (
+    SELECT
+        toc.discid,
+        t.name AS track,
+        a.sort_name AS name,
+        to_recording_url(r.gid) AS url
+    FROM
+        toc
+        INNER JOIN medium_cdtoc AS mc ON mc.cdtoc = toc.id
+        INNER JOIN track AS t ON t.medium = mc.medium
+        INNER JOIN recording AS r ON t.recording = r.id
+        INNER JOIN l_artist_recording AS lar ON lar.entity1 = r.id
+        INNER JOIN conductor AS a ON lar.entity0 = a.id
+    WHERE
+        t.position = 6
+    GROUP BY
+        toc.discid, t.name, a.sort_name, r.gid
+)
+SELECT
+    to_cdtoc_url(discid) AS discid,
+    ARRAY_AGG(track) AS track_names
+FROM
+    results
+GROUP BY
+    discid
+HAVING
+    COUNT(DISTINCT(url)) > 1
+;
